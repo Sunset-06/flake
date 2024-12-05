f8252a801956621d8af7f4676001bcf24677e6ef
Sunset-06 <nilay.sabnis05@gmail.com>
2024-12-05 14:15:00 +0530

    added a few more functions

--- src/Chip8.h
+++ src/Chip8.h
@@ -12,27 +12,28 @@
 #define FONTSET_SIZE 80
 
 extern uint8_t memory[4096];
-extern uint32_t screen[64 * 32];
 extern uint16_t pc;
 extern uint16_t I;
-extern uint16_t stack[16];
-extern uint8_t delayTimer;
-extern uint8_t soundTimer;
 extern unsigned int registers[16];
-
+extern uint16_t stack[16];
 extern uint8_t sp;
-extern uint16_t opcode;
-extern uint8_t keypad[16];
 extern uint8_t fontset[FONTSET_SIZE];
 
-/* 
-// Function prototypes
-void fetch();
-void operate();
-void CPUCycle();
+extern uint32_t screen[64 * 32];
+extern uint8_t delayTimer;
+extern uint8_t soundTimer;  
+extern uint16_t opcode;
+extern uint8_t keypad[16];
+
+
 void LoadRom(char filename[]);
-void drawScreen();
+//SDL functions
 void initializeSDL();
+void drawScreen();
 void endSDL();
+/* 
+void fetch();
+void execute();
+void CPUCycle();
  */
 #endif 
